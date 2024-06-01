d346f7d31d2e8379ac742c64b2fabfbabd9d82bb
veeru <veeru@gmail.com>
2024-06-01 09:41:35 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Home from './components/Home'
+
+import './App.css'
+
+const App = () => <Home />
+
+export default App
+
+// import './App.css'
+
+// //This is the list (static data) used in the application. You can move it to any component if needed.
+
+// const categoriesList = [
+//   {id: 'ALL', displayText: 'All'},
+//   {id: 'STATIC', displayText: 'Static'},
+//   {id: 'RESPONSIVE', displayText: 'Responsive'},
+//   {id: 'DYNAMIC', displayText: 'Dynamic'},
+//   {id: 'REACT', displayText: 'React'},
+// ]
+
+// // Replace your code here
+// const App = () => <h1>Hello World</h1>
+
+// export default App
